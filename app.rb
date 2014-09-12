13e2d82281cd2fac4ab1264d311faefd1228cbee
Max <maxrogers90@gmail.com>
2014-09-12 14:09:36 -0400

    try again

--- app.rb
+++ app.rb
@@ -36,13 +36,13 @@ get '/auth/twitter/callback' do
   session[:admin] = true
   session[:username] = env['omniauth.auth']['info']['name']
 
-  session[:client] = Twitter::REST::Client.new do |config|
+  client = Twitter::REST::Client.new do |config|
     config.consumer_key        = "p2xojaAqxCshYaAraH4otANCr"
     config.consumer_secret     = "TKzK7CRkr71oLRa102V6DF5VQY5BAltvqrkHZWxhXouybcNAxZ"
     config.access_token        = env['omniauth.auth'][:credentials][:token]
     config.access_token_secret = env['omniauth.auth'][:credentials][:secret]
   end
-  session[:client].update("I'm tweeting with @gem!")
+  client.update("I'm tweeting with @gem!")
   "#{env['omniauth.auth']}<br><br>#{env['omniauth.auth'][:credentials][:token]}<br>#{env['omniauth.auth'][:credentials][:secret]}"
 end
 
