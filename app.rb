1c81e940c99d4b07ecf31610a9d67fe2966192af
Max <maxrogers90@gmail.com>
2014-09-12 12:51:58 -0400

    trying to post to heroku

--- app.rb
+++ app.rb
@@ -5,8 +5,38 @@ require "./models.rb"
 configure do
   enable :sessions
   use OmniAuth::Builder do
-    provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+    provider :twitter, ENV['p2xojaAqxCshYaAraH4otANCr'], ENV['TKzK7CRkr71oLRa102V6DF5VQY5BAltvqrkHZWxhXouybcNAxZ']
   end
+end
+
+helpers do
+  # define a current_user method, so we can be sure if an user is authenticated
+  def current_user
+    !session[:uid].nil?
+  end
+end
+
+before do
+  # we do not want to redirect to twitter when the path info starts
+  # with /auth/
+  pass if request.path_info =~ /^\/auth\//
+
+  # /auth/twitter is captured by omniauth:
+  # when the path info matches /auth/twitter, omniauth will redirect to twitter
+  redirect to('/auth/twitter') unless current_user
+end
+
+get '/auth/twitter/callback' do
+  # probably you will need to create a user in the database too...
+  session[:uid] = env['omniauth.auth']['uid']
+  # this is the main endpoint to your application
+  puts "#{sesssion[:uid]}"
+  redirect to('/')
+end
+
+get '/auth/failure' do
+  # omniauth redirects to /auth/failure when it encounters a problem
+  # so you can implement this as you please
 end
 
 get "/" do
