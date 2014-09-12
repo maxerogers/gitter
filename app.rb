16372fe856f969553e187587e65add88fca7200c
Max <maxrogers90@gmail.com>
2014-09-12 14:01:07 -0400

    rwar

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require 'omniauth-twitter'
+require 'twitter'
 require "./models.rb"
 
 #setup
@@ -35,7 +36,12 @@ get '/auth/twitter/callback' do
   session[:admin] = true
   session[:username] = env['omniauth.auth']['info']['name']
 
-  
+  session[:client] = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "p2xojaAqxCshYaAraH4otANCr"
+    config.consumer_secret     = "TKzK7CRkr71oLRa102V6DF5VQY5BAltvqrkHZWxhXouybcNAxZ"
+    config.access_token        = env['omniauth.auth'][:credentials][:token]
+    config.access_token_secret = env['omniauth.auth'][:credentials][:secret]
+  end
   "#{env['omniauth.auth']}<br><br>#{env['omniauth.auth'][:credentials][:token]}<br>#{env['omniauth.auth'][:credentials][:secret]}"
 end
 
