a843fc6fae9c9290516b2bf724620a5a0d301ad0
Max <maxrogers90@gmail.com>
2014-09-21 10:26:26 -0400

    for the tests

--- app.rb
+++ app.rb
@@ -202,7 +202,7 @@ def reload_server
       r.last_sha = latest_sha
       r.save
       str += "#{json['files']}<br><br>"
-      if json["files"] == "[]"
+      if json["files"].empty?
         #str += "#{json['files']}<br><br>"
         Language.update_all(hourly_count: 0)
         json["files"].each do |f|
